373595d017d42913fbe4b961a44950c31ac2184f
Umit Akirmak <umitakirmak@gmail.com>
2024-09-27 11:23:44 -0700

    created the basic routes including home, contact, and about. also I implemented a mock quiz that display results

---
+++ quiz-app/src/lib/Footer.svelte
@@ -0,0 +1,13 @@
+<footer class="footer">
+	<p>&copy; 2024 EasyPath Web App. All rights reserved.</p>
+</footer>
+
+<style>
+	.footer {
+		text-align: center;
+		padding: 1rem;
+		background-color: #f8f9fa;
+		color: #333;
+		margin-bottom: 2rem;
+	}
+</style>
