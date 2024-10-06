76c158a13ecae9d70915c906c570a4847b22b566
Hoksolinvan <chhunhoksolinvan.prof@gmail.com>
2024-10-05 18:12:30 -0700

    Update config.cjs

--- quiz-app/backend/config.cjs
+++ quiz-app/backend/config.cjs
@@ -2,11 +2,10 @@ require("dotenv").config();
 const pgp = require('pg-promise')();
 
 
-const PostGresDB = `postgresql://postgres:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
+const PostGresDB = `postgresql://postgres:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
 
 
 const connection = pgp({PostGresDB})
-
 
 module.exports = connection;
