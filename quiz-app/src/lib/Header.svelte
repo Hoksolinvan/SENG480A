bb14448a0fa21fa61f7cb0a5fd7a92bcde0cc5ce
harrylam-quay <lamtheduong@yahoo.com.vn>
2024-10-07 02:44:30 -0700

    Added drop-down menu.

--- quiz-app/src/lib/Header.svelte
+++ quiz-app/src/lib/Header.svelte
@@ -4,28 +4,22 @@
 
 <header class="header">
 	<div class="header-container">
-		<nav class="nav">
-			<ul>
-				<li><img src="/logo.png" alt="logo" class="logo" /></li>
-				<li><a href="/login" class = "button">
-					<i class="material-icons">list</i>
-				</a></li>
-			</ul>
-			
-		</nav>
+		<img src="/logo.png" alt="logo" class="logo" />
 		
-		<!-- <nav class="nav">
-			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/about">About</a></li>
-				<li><a href="/contact">Contact</a></li>
-			</ul>
-		</nav> -->
 		<nav class="nav">
-			<ul>
-				<li><a href="/login" class = "button">LOG IN</a></li>
-				<li><a href="/signup" class = "button">SIGN UP</a></li>
-			</ul>
+		<ul>
+			<li class = "button"><a href="/login">LOG IN</a></li>
+			<li class = "button"><a href="/signup">SIGN UP</a></li>
+			<li>
+				<div class="dropdown"><i class="material-icons">list</i>
+					<div class="dropdown-content">
+						<a href="/">Home</a>
+						<a href="/about">About</a>
+						<a href="/contact">Contact</a>
+				</div>
+				</div> 
+			</li>
+		</ul>
 		</nav>
 	</div>
 </header>
@@ -41,6 +35,14 @@
 		font-weight: bold;
 	}
 
+	.material-icons {
+		color: #007bff;
+		font-size: 3rem;
+		border: solid;
+		border-color: #007bff;
+		border-radius: 10px;
+	}
+
 	.nav ul {
 		display: flex;
 		list-style-type: none;
@@ -49,6 +51,7 @@
 
 	.nav ul li {
 		margin: 0 0.25rem;
+		padding: auto;
 	}
 
 	.header-container {
@@ -62,15 +65,35 @@
 		border-radius: 10px;
 	}
 
-	/* .nav ul {
-		display: flex;
-		list-style-type: none;
-		padding: 0;
+		/* The container <div> - needed to position the dropdown content */
+	.dropdown {
+		position: relative;
+		display: inline-block;
 	}
 
-	.nav ul li {
-		margin: 0 1rem;
-	} */
+		/* Dropdown Content (Hidden by Default) */
+	.dropdown-content {
+		display: none;
+		position: absolute;
+		background-color: #f1f1f1;
+		min-width: 160px;
+		box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+		z-index: 1;
+	}
+
+	/* Links inside the dropdown */
+	.dropdown-content a {
+		color: black;
+		padding: 12px 16px;
+		text-decoration: none;
+		display: block;
+	}
+
+	/* Change color of dropdown links on hover */
+	.dropdown-content a:hover {background-color: #ddd;}
+
+	/* Show the dropdown menu on hover */
+	.dropdown:hover .dropdown-content {display: block;}
 
 	@media (min-width: 768px) {
 		.header-container {
