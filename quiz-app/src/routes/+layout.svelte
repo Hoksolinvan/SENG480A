ee582bae2cd704593b6ddc589628d9c6cfcdaadc
Hoksolinvan <chhunhoksolinvan.prof@gmail.com>
2024-10-06 21:30:06 -0700

    Update +layout.svelte

--- quiz-app/src/routes/+layout.svelte
+++ quiz-app/src/routes/+layout.svelte
@@ -23,9 +23,8 @@
 		flex-direction: column;
 		min-height: 100vh;
 		padding: 1rem;
-		max-width: 1000px;
+		max-width: 400%;
 		margin: 2rem auto;
-		
 		background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
 		background-size: 400% 400%;
 		animation: gradient 15s ease infinite;
