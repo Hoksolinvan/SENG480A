29ae2ce8e65c4f90607938f2d040c1b18a1337bb
harrylam-quay <60025081+harrylam-quay@users.noreply.github.com>
2024-11-28 19:12:17 -0800

    Button color change

--- quiz-app/src/routes/documents/+page.svelte
+++ quiz-app/src/routes/documents/+page.svelte
@@ -63,8 +63,8 @@
 
 <div class="space-y-8 max-w-4xl mx-auto p-6">
   <button 
-    class="w-fit px-4 py-2 bg-blue-900 text-white rounded-lg shadow-sm 
-    hover:bg-blue-800 transform hover:scale-105 transition-all
+    class="w-fit px-4 py-2 bg-darkgreen text-white rounded-lg shadow-sm 
+    hover:bg-green-800 transform hover:scale-105 transition-all
     duration-300text-sm font-medium border border-gray-200"
     onclick="window.location.href = './dashboard';"
   >
