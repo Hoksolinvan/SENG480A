7f3ec173a35ccc62d9cb6122c3ebbe98cd46f434
Umit Akirmak <umitakirmak@gmail.com>
2024-12-06 14:56:17 -0800

    changed demo profile names and pics -> Pat and Emma

--- quiz-app/src/routes/profile/profileList.js
+++ quiz-app/src/routes/profile/profileList.js
@@ -1,20 +1,20 @@
 // Convoluted, hard-coded profile database for demo
 
 let profileDetailList = [
-        {
-                email: "demo@easypath.ca",
-                first_name: "Avery",
-                last_name: "Thompson",
-                PFP: "./s2.png",
-                courses: []
-        },
-        {
-                email: "demo2@easypath.ca",
-                first_name: "John",
-                last_name: "Patel",
-                PFP: "./s3.png",
-                courses: []
-        }
+	{
+		email: 'demo@easypath.ca',
+		first_name: 'Pat',
+		last_name: '',
+		PFP: './pat.png',
+		courses: []
+	},
+	{
+		email: 'demo2@easypath.ca',
+		first_name: 'Emma',
+		last_name: '',
+		PFP: './s2.png',
+		courses: []
+	}
 ];
 
 export default profileDetailList;
